5497729c24269868a24f4471c69c74c7c228500b
porto.vga <porto.vga@gmail.com>
2011-01-02 11:39:21 +0000

    Estructura de las clases Regla y TablaReglas creada

---
+++ src/wangmendel/TablaReglas.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package wangmendel;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author porto
+ */
+public class TablaReglas {
+    
+    private ArrayList<Regla> listaReglas;
+
+    public void addRegla()
+    {
+
+    }
+
+    public void imprimir()
+    {
+
+    }
+
+}
