71ac82c5552f6b3f804ea3b4fc4bcb6370824442
porto.vga@gmail.com <porto.vga@gmail.com@88581700-3236-d82f-1186-4258e50bd3ff>
2011-01-02 11:39:21 +0000

    Estructura de las clases Regla y TablaReglas creada

---
+++ trunk/src/wangmendel/Regla.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package wangmendel;
+
+/**
+ *
+ * @author porto
+ */
+public class Regla {
+
+    private Float blueRed;
+    private Float greenBlue;
+    private Float redGreen;
+    private Integer consecuente;
+    private Float peso;
+
+
+    public Regla(Float br, Float gb, Float rg, Float peso)
+    {
+        
+    }
+
+    public Float getBlueRed() {
+        return blueRed;
+    }
+
+    public Integer getConsecuente() {
+        return consecuente;
+    }
+
+    public Float getGreenBlue() {
+        return greenBlue;
+    }
+
+    public Float getPeso() {
+        return peso;
+    }
+
+    public Float getRedGreen() {
+        return redGreen;
+    }
+
+    public void setConsecuente(Integer consecuente) {
+        this.consecuente = consecuente;
+    }
+
+    
+
+    public void construirID(String id)
+    {
+        
+    }
+
+}
