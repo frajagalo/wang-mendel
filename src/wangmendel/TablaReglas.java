fc7e95ba2ae4abceb38083e498e84a1d1c6e3264
porto.vga <porto.vga@gmail.com>
2011-01-02 18:36:49 +0000

    Implementados métodos para añadir una regla e imprimir las reglas existentes en el hashmap por pantalla. Se han añadido también dos métodos a mayores, uno para pasar cada elemento del antecedente a texto, y el otro para pasar el consecuente a texto

--- src/wangmendel/TablaReglas.java
+++ src/wangmendel/TablaReglas.java
@@ -5,7 +5,9 @@
 
 package wangmendel;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  *
@@ -13,16 +15,74 @@ import java.util.ArrayList;
  */
 public class TablaReglas {
     
-    private ArrayList<Regla> listaReglas;
+    private HashMap<String,Regla> listaReglas;
 
-    public void addRegla()
+    public TablaReglas()
     {
+        this.listaReglas=new HashMap<String,Regla>();
+    }
 
+    public void addRegla(Regla regla)
+    {
+        String id=regla.construirID();
+        if(listaReglas.containsKey(id))
+        {
+            if(listaReglas.get(id).getPeso()< regla.getPeso())
+                listaReglas.put(id, regla);
+        }
+        else
+            listaReglas.put(id, regla);
+
+        System.out.println(id);
     }
 
     public void imprimir()
     {
-
+        if(listaReglas.isEmpty())
+            System.out.println("No se ha obtenido ninguna regla como resultado");
+        else
+        {
+            Regla regla;
+            
+            Iterator it=listaReglas.entrySet().iterator();
+            while(it.hasNext())
+            {
+                Map.Entry e=(Map.Entry)it.next();
+                regla=(Regla)e.getValue();
+                System.out.println("Regla"+regla.getPeso());
+                System.out.println("if (blueRed is "+textoAntecedente(regla.getBlueRed())
+                                   +") and (greenBlue is "+textoAntecedente(regla.getGreenBlue())
+                                   +") and (redGreen is "+textoAntecedente(regla.getRedGreen())
+                                   +") then (tipo is "+textoConsecuente(regla.getConsecuente())
+                                   +")  #Peso="+regla.getPeso());
+            }
+        }
+            
     }
 
+    private String textoAntecedente(short i)
+    {
+        if (i == Parametros.PEQUENO)
+            return "pequeno";
+        else if (i == Parametros.MEDIANO)
+            return "mediano";
+        else if (i == Parametros.GRANDE)
+            return "grande";
+        else
+            return "indefinido";
+    }
+
+    private String textoConsecuente(short i)
+    {
+        if (i == 1)
+            return "vermello";
+        else if (i == 2)
+            return "verde";
+        else if (i == 3)
+            return "branco";
+        else if (i == 4)
+            return "azul";
+        else
+            return "indefinido";
+    }
 }
