d8ced3a277c3bc55fc80ebd6864b421c29083c9b
porto.vga <porto.vga@gmail.com>
2011-01-02 13:31:04 +0000

    Clase Datos implementada y probada, primera versión: 	- lee línea a línea el fichero 	- separa en tokens a partir de las divisiones en comas 	- parsea tokens de string a Integer y Float, según el caso 	- guarda datos en un registro, y lo añade a la lista
    Se ha decidido leer los datos a partir de llamadas al constructor y dejar públic el método de obtenerDatos() para posibles actualizaciones de los datos.

--- src/entrada/Datos.java
+++ src/entrada/Datos.java
@@ -5,6 +5,10 @@
 
 package entrada;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -18,17 +22,52 @@ public class Datos {
 
     public Datos(String fichero)
     {
+        this.nombreFichero=fichero;
+        this.registros=new ArrayList<Registro>();
+
+        try{
+            obtenerDatos();
+        } catch (FileNotFoundException ex) {
+            System.out.println("No se encuentra el fichero " + nombreFichero);
+            System.exit(1);
+        } catch (IOException ex) {
+            System.out.println("Se ha producido un error al leer " + nombreFichero);
+            System.exit(1);
+        }
     }
 
-    public void obtenerDatos()
+    public void obtenerDatos() throws FileNotFoundException, IOException
     {
+        FileReader fr=new FileReader(this.nombreFichero);
+        BufferedReader br= new BufferedReader(fr);
+        Registro reg;
 
+        br.readLine();                                                          // saltamos cabecera
+
+        String linea=br.readLine();
+        while(linea != null)
+        {
+            String[] tokens=linea.split(",");
+            
+            reg=new Registro();
+            reg.setBlueRed(Float.valueOf(tokens[1]));
+            reg.setGreenBlue(Float.valueOf(tokens[2]));
+            reg.setRedGreen(Float.valueOf(tokens[3]));
+            reg.setTipo(Integer.parseInt(tokens[4].trim()));
+            this.registros.add(reg);
+            
+
+            linea=br.readLine();
+        }
+
+
+        fr.close();
+        br.close();
     }
 
     public ArrayList<Registro> getRegistros() {
         return registros;
     }
-
 
 
 }
