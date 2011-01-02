2934e95bf65a7c15cb59f45b77890fcc7fc358e8
porto.vga@gmail.com <porto.vga@gmail.com@88581700-3236-d82f-1186-4258e50bd3ff>
2011-01-02 11:40:54 +0000

    Estructura de las clases Registro y Datos creada

---
+++ trunk/src/entrada/Datos.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entrada;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author porto
+ */
+public class Datos {
+
+    private String nombreFichero;
+    private ArrayList<Registro> registros;
+
+    public Datos(String fichero)
+    {
+    }
+
+    public void obtenerDatos()
+    {
+
+    }
+
+    public ArrayList<Registro> getRegistros() {
+        return registros;
+    }
+
+
+
+}
