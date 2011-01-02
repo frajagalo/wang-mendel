c86a9216539c29090d7c63958b12b5eb989916e2
porto.vga <porto.vga@gmail.com>
2011-01-02 11:40:54 +0000

    Estructura de las clases Registro y Datos creada

---
+++ src/entrada/Registro.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package entrada;
+
+/**
+ *
+ * @author porto
+ */
+public class Registro {
+
+    private Float blueRed;
+    private Float greenBlue;
+    private Float redGreen;
+    private Integer tipo;
+
+
+    
+    public Float getBlueRed() {
+        return blueRed;
+    }
+
+    public void setBlueRed(Float blueRed) {
+        this.blueRed = blueRed;
+    }
+
+    public Float getGreenBlue() {
+        return greenBlue;
+    }
+
+    public void setGreenBlue(Float greenBlue) {
+        this.greenBlue = greenBlue;
+    }
+
+    public Float getRedGreen() {
+        return redGreen;
+    }
+
+    public void setRedGreen(Float redGreen) {
+        this.redGreen = redGreen;
+    }
+
+    public Integer getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(Integer tipo) {
+        this.tipo = tipo;
+    }
+
+
+}
